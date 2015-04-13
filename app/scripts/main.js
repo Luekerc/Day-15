a123991bc3892d148e3fdb3eb7655177bb069ed2
Charles Lueker <charleslueker@yahoo.com>
2015-04-13 02:50:17 -0500

    added drop down buttonand trying to push to tiny pizza server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,29 @@
 /* jshint devel:true */
-$(document).ready(onReady);
+// $(document).ready(onReady);
+// function onReady() {
+// }
+$(document).ready(function(){
 
-function onReady() {
-	$('.searchbutton').on('click', onSearchButtonClick);
+var getMessages = function() {
+	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http', 	
+		function(movie) {
+			render(movie);
+		},
+		'json'
+	);
+};
+
+var render = function(movie) {
+	$('.watched').append(this);
+
+	};
+
+$('.searchbutton').on('click', onSearchButtonClick);
 	
-	function onSearchButtonClick (){
-	// $('#results').html($('#search-box').val());	
+function onSearchButtonClick(){	
 	movieSearch($('.search-box').val());	
-	}
 }
+
 function movieSearch(query) {
 	$.get('http://www.omdbapi.com',
 		{
@@ -18,33 +33,51 @@ function movieSearch(query) {
 		'json'
 		);
 }
+
 function onResultsReceived(data) {
-	// console.log(onResultsReceived);
-	console.log(data);	
-	// consoles.log(data.Search[2].Title);
-	
+		console.log(data);
 	for (var i=0; i<data.Search.length; i++){
 		var movieInfo=data.Search[i];
-		var newRow = makeRow(movieInfo);
-		$('#table').append(newRow);
+		// if(movieInfo.Type=="movie"){
+			var newRow = makeRow(movieInfo);
+			$('#table').append(newRow);
+		// }	
 	}	
 	function makeRow(data){
-		console.log(data);
-		return '<tr><td>' + data.Title + '</td>\
+		return '<tr class="tableRow"><td>' + data.Title + '</td>\
 		<td>'+data.Year+'</td></tr>';
 		}
-		$("tr").click(function watchlist(){
-			// console.log(this);
-			$('.towatch').append(this);
-			$(this).off('click');
-			$(this).on('click', onClickB);
 
-			function onClickB (){
-			$('.watched').append(this);
-			}
-		})
+	$("tr").click(function watchlist(){
+		$('.towatch').append(this);
+		console.log(this);
+		//The .off() is here b/c otherwise a duplicate
+		//object is created when onClickB() is run.
+		$(this).off('click');
+		$(this).on('click', onClickB);
+		//send "this" to storage for later retrieval
+		// console.log(this);
+		// console.log(this);
 
-
-
-		
+		function onClickB(){
+		var movie = $('.watched').append(this);
+		//send "this" to storage for later retrieval
+		console.log(movie);
+		$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http',
+			{
+				movie: {
+					movie: movie,
+					}
+			}, 
+				function(movie){
+					console.log(movie);
+					render(movie);
+				},
+				'json'
+			);
 	}
+		
+	})	
+}
+
+});
