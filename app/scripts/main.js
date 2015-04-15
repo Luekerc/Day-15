3a68934044d84a841cde87b6b19f1384e2c17df6
Charles Lueker <charleslueker@yahoo.com>
2015-04-14 21:44:04 -0500

    added sorting by type with drop down but the movie and series wont push to the next array.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,20 +3,26 @@
 // function onReady() {
 // }
 $(document).ready(function(){
-
+var movie;
 var getMessages = function() {
 	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http', 	
 		function(movie) {
-			render(movie);
+			render(this);
+			// console.log(movie);
+			// console.log(this);
 		},
 		'json'
 	);
+
 };
+getMessages();
 
 var render = function(movie) {
 	$('.watched').append(this);
-
+	// console.log(this);
+	// console.log(movie);
 	};
+render();
 
 $('.searchbutton').on('click', onSearchButtonClick);
 	
@@ -32,22 +38,70 @@ function movieSearch(query) {
 		onResultsReceived,
 		'json'
 		);
-}
+};
 
 function onResultsReceived(data) {
-		console.log(data);
+		// console.log(data);
+		var movieInfo;
 	for (var i=0; i<data.Search.length; i++){
-		var movieInfo=data.Search[i];
+		movieInfo=data.Search[i];
 		// if(movieInfo.Type=="movie"){
 			var newRow = makeRow(movieInfo);
 			$('#table').append(newRow);
-		// }	
+		// }
 	}	
+	// console.log(data);
+	var movieType;
+	$('#All').click(function sortAll(){
+		$('#table').html("");
+		movieSearch($('.search-box').val());
+		console.log(data);
+		for (var i=0; i<data.Search.length; i++){
+		movieInfo=data.Search[i];	
+		$('#table').append(movieInfo);
+		}
+	});
+	//Showing only Movies - the hard way
+	$('#Movie').click(function sortMovie(){
+		$('#table').html("");
+		console.log(data);
+		for (var i=0; i<data.Search.length; i++){
+		movieInfo=data.Search[i];
+		console.log(movieInfo);
+		if(movieInfo.Type=="movie"){
+			var newRow = makeRow(movieInfo);
+			$('#table').append(newRow);
+			console.log(movieInfo);
+			}
+		}
+	});
+	
+	$('#Series').click(function sortSeries(){
+		$('#table').html("");
+		console.log(data);
+		for (var i=0; i<data.Search.length; i++){
+			movieInfo=data.Search[i];
+			if(movieInfo.Type=="series"){
+				var newRow = makeRow(movieInfo);
+				$('#table').append(newRow);
+				console.log(movieInfo);
+			}
+		}
+	});
+
+
+
+
+	//this creates the table row to be appended into #table
 	function makeRow(data){
 		return '<tr class="tableRow"><td>' + data.Title + '</td>\
 		<td>'+data.Year+'</td></tr>';
-		}
+		};
 
+	//function that shows by Type when you click drop down #Movie
+
+
+	//this appends the clicked element into .towatch
 	$("tr").click(function watchlist(){
 		$('.towatch').append(this);
 		console.log(this);
@@ -60,24 +114,31 @@ function onResultsReceived(data) {
 		// console.log(this);
 
 		function onClickB(){
-		var movie = $('.watched').append(this);
+		$('.watched').append(this);
+	
 		//send "this" to storage for later retrieval
-		console.log(movie);
+		console.log(this);
 		$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http',
 			{
 				movie: {
-					movie: movie,
+					this: "this",
 					}
 			}, 
 				function(movie){
-					console.log(movie);
 					render(movie);
 				},
 				'json'
 			);
-	}
+		//This is the html and data to append to .watched upon page load
+		console.log(this);
+
+		}
 		
 	})	
 }
+	
 
 });
+
+
+
