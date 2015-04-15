c50cf74e99c83510307d7b7da3985acbcc2b755f
Charles Lueker <charleslueker@yahoo.com>
2015-04-14 23:12:27 -0500

    This was working now the old html wont clear when I sort

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,29 @@
 /* jshint devel:true */
-// $(document).ready(onReady);
-// function onReady() {
-// }
-$(document).ready(function(){
-var movie;
-var getMessages = function() {
-	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http', 	
-		function(movie) {
-			render(this);
-			// console.log(movie);
-			// console.log(this);
-		},
-		'json'
-	);
+$(document).ready(onReady);
+function onReady(){
+	var movie;
+	var movieInfo;
+	var movieType;
 
-};
-getMessages();
+// var getMessages = function() {
+// 	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http', 	
+// 		function(movie) {
+// 			render(movie);
+// 			// console.log(movie);
+// 		},
+// 		'json'
+// 	);
+// };
+// getMessages();
 
-var render = function(movie) {
-	$('.watched').append(this);
-	// console.log(this);
-	// console.log(movie);
-	};
-render();
+// var render = function(movie) {
+// 	$('.watched').append(movie);
+// 	console.log(movie);
+// 	};
+// render();
 
-$('.searchbutton').on('click', onSearchButtonClick);
-	
-function onSearchButtonClick(){	
-	movieSearch($('.search-box').val());	
-}
-
+$('#searchbutton').on('click', onSearchButtonClick);
+$('#table').html("");
 function movieSearch(query) {
 	$.get('http://www.omdbapi.com',
 		{
@@ -40,56 +34,58 @@ function movieSearch(query) {
 		);
 };
 
+function onSearchButtonClick(){	
+	movieSearch($('.search-box').val());	
+};
+$("#clearresults").click(function(){
+	$('#table').html("");
+});
+
 function onResultsReceived(data) {
-		// console.log(data);
-		var movieInfo;
+	
 	for (var i=0; i<data.Search.length; i++){
 		movieInfo=data.Search[i];
-		// if(movieInfo.Type=="movie"){
-			var newRow = makeRow(movieInfo);
-			$('#table').append(newRow);
-		// }
+		var newRow = makeRow(movieInfo);
+		$('#table').append(newRow);
 	}	
-	// console.log(data);
-	var movieType;
+	//Show all Types again: movies and series
 	$('#All').click(function sortAll(){
 		$('#table').html("");
 		movieSearch($('.search-box').val());
 		console.log(data);
 		for (var i=0; i<data.Search.length; i++){
-		movieInfo=data.Search[i];	
-		$('#table').append(movieInfo);
+			movieInfo=data.Search[i];	
+			$('#table').append(movieInfo);
+			console.log(this);
 		}
 	});
-	//Showing only Movies - the hard way
+
+	//Showing only movies - the hard way
 	$('#Movie').click(function sortMovie(){
-		$('#table').html("");
-		console.log(data);
+		$("#table").html("");
+		movieSearch($('.search-box').val());
 		for (var i=0; i<data.Search.length; i++){
-		movieInfo=data.Search[i];
-		console.log(movieInfo);
-		if(movieInfo.Type=="movie"){
-			var newRow = makeRow(movieInfo);
-			$('#table').append(newRow);
-			console.log(movieInfo);
+			movieInfo=data.Search[i];
+			if(movieInfo.Type==="movie"){
+				console.log(movieInfo.Type);
+				var newRow = makeRow(movieInfo);
+				$('#table').append(newRow);
 			}
 		}
 	});
-	
+	//Show only series - the hard way
 	$('#Series').click(function sortSeries(){
-		$('#table').html("");
-		console.log(data);
+		$("#table").html("");
+		movieSearch($('.search-box').val());
 		for (var i=0; i<data.Search.length; i++){
 			movieInfo=data.Search[i];
 			if(movieInfo.Type=="series"){
+				console.log(movieInfo.Type);
 				var newRow = makeRow(movieInfo);
 				$('#table').append(newRow);
-				console.log(movieInfo);
 			}
 		}
 	});
-
-
 
 
 	//this creates the table row to be appended into #table
@@ -97,9 +93,6 @@ function onResultsReceived(data) {
 		return '<tr class="tableRow"><td>' + data.Title + '</td>\
 		<td>'+data.Year+'</td></tr>';
 		};
-
-	//function that shows by Type when you click drop down #Movie
-
 
 	//this appends the clicked element into .towatch
 	$("tr").click(function watchlist(){
@@ -110,18 +103,18 @@ function onResultsReceived(data) {
 		$(this).off('click');
 		$(this).on('click', onClickB);
 		//send "this" to storage for later retrieval
-		// console.log(this);
-		// console.log(this);
+
+
 
 		function onClickB(){
-		$('.watched').append(this);
-	
-		//send "this" to storage for later retrieval
-		console.log(this);
-		$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http',
+			$('.watched').append(this);
+			//send "this" to storage for later retrieval
+			console.log(this);
+			$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http',
 			{
 				movie: {
-					this: "this",
+	//I've tried various movie: values - movie: "movie", "this", etc...
+					movie: this,
 					}
 			}, 
 				function(movie){
@@ -129,16 +122,12 @@ function onResultsReceived(data) {
 				},
 				'json'
 			);
-		//This is the html and data to append to .watched upon page load
-		console.log(this);
-
+		//This should be the html and data to append to .watched upon page load
+		console.log(movie);
 		}
-		
 	})	
 }
-	
-
-});
+};
 
 
 
