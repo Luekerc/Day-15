34b52e7869df3a95e7bbff51a58d0b931430d27b
Charles Lueker <charleslueker@yahoo.com>
2015-04-23 11:16:53 -0500

    added the get and post to the towatch list but i still need to delete it when i transfer it to the watched list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,26 +5,48 @@ function onReady(){
 	var movieInfo;
 	var movieType;
 
-// var getMessages = function() {
-// 	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http', 	
-// 		function(movie) {
-// 			render(movie);
-// 			// console.log(movie);
-// 		},
-// 		'json'
-// 	);
-// };
-// getMessages();
+var getMovies = function() {
+	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlesmovies-http', 	
+		function(movie) {
+			render(movie);
+			console.log(movie);
+		},
+		'json'
+	);
+	console.log(this);
+};
+getMovies();
 
-// var render = function(movie) {
-// 	$('.watched').append(movie);
-// 	console.log(movie);
-// 	};
-// render();
+var render = function(movie) {
+	for(var i=0; i<movie.length;i++){
+		$('.watched').append("<tr>"+movie[i].movie.movie+"</tr>");	
+	}
+};
+
+
+var getTowatch = function() {
+	$.get('https://tiny-pizza-server.herokuapp.com/collections/charlestowatch-http', 	
+		function(movie) {
+			setTowatch(movie);
+			console.log(movie);
+		},
+		'json'
+	);
+	console.log(this);
+};
+getTowatch();
+
+var setTowatch = function(movie) {
+	for(var i=0; i<movie.length;i++){
+		$('.towatch').append("<tr>"+movie[i].movie.movie+"</tr>");	
+	}
+};
 
 $('#searchbutton').on('click', onSearchButtonClick);
-$('#table').html("");
+
 function movieSearch(query) {
+	$('#table').html("");
+	console.log("gotten");
 	$.get('http://www.omdbapi.com',
 		{
 		s: query,
@@ -37,8 +59,10 @@ function movieSearch(query) {
 function onSearchButtonClick(){	
 	movieSearch($('.search-box').val());	
 };
+
 $("#clearresults").click(function(){
 	$('#table').html("");
+	$('.search-box').val("");
 });
 
 function onResultsReceived(data) {
@@ -49,43 +73,46 @@ function onResultsReceived(data) {
 		$('#table').append(newRow);
 	}	
 	//Show all Types again: movies and series
-	$('#All').click(function sortAll(){
-		$('#table').html("");
-		movieSearch($('.search-box').val());
-		console.log(data);
-		for (var i=0; i<data.Search.length; i++){
-			movieInfo=data.Search[i];	
-			$('#table').append(movieInfo);
-			console.log(this);
-		}
-	});
+	// $('#All').click(function sortAll(){
+	// 	$('#table').html("");
+	// 	movieSearch($('.search-box').val());
+	// 	console.log(data);
+	// 	for (var i=0; i<data.Search.length; i++){
+	// 		movieInfo=data.Search[i];	
+	// 		$('#table').append(movieInfo);
+	// 		console.log(this);
+	// 	}
+	// });
 
 	//Showing only movies - the hard way
-	$('#Movie').click(function sortMovie(){
-		$("#table").html("");
-		movieSearch($('.search-box').val());
-		for (var i=0; i<data.Search.length; i++){
-			movieInfo=data.Search[i];
-			if(movieInfo.Type==="movie"){
-				console.log(movieInfo.Type);
-				var newRow = makeRow(movieInfo);
-				$('#table').append(newRow);
-			}
-		}
-	});
+	// $('#Movie').click(function sortMovie(){
+	// 	$("#table").html("");
+	// 	movieSearch($('.search-box').val());
+	// 	for (var i=0; i<data.Search.length; i++){
+	// 		movieInfo=data.Search[i];
+	// 		if(movieInfo.Type==="movie"){
+	// 			console.log(movieInfo.Type);
+	// 			var newRow = makeRow(movieInfo);
+	// 			$("#table").append(newRow);
+	// 		}
+
+	// 	}
+	// });
 	//Show only series - the hard way
-	$('#Series').click(function sortSeries(){
-		$("#table").html("");
-		movieSearch($('.search-box').val());
-		for (var i=0; i<data.Search.length; i++){
-			movieInfo=data.Search[i];
-			if(movieInfo.Type=="series"){
-				console.log(movieInfo.Type);
-				var newRow = makeRow(movieInfo);
-				$('#table').append(newRow);
-			}
-		}
-	});
+	// $('#Series').click(function sortSeries(){
+		
+	// 	movieSearch($('.search-box').val());
+	// 	$("#table").html("");
+	// 	for (var i=0; i<data.Search.length; i++){
+	// 		movieInfo=data.Search[i];
+	// 		if(movieInfo.Type=="series"){
+	// 			console.log(movieInfo.Type);
+	// 			var newRow = makeRow(movieInfo);
+	// 			$("#table").append(newRow);
+	// 		}
+
+	// 	}
+	// });
 
 
 	//this creates the table row to be appended into #table
@@ -98,23 +125,32 @@ function onResultsReceived(data) {
 	$("tr").click(function watchlist(){
 		$('.towatch').append(this);
 		console.log(this);
+		$.post('https://tiny-pizza-server.herokuapp.com/collections/charlestowatch-http',
+			{
+				movie: {
+					movie: this.innerHTML,
+					}
+			}, 
+				function(movie){
+					setTowatch(movie);
+				},
+				'json'
+			);
 		//The .off() is here b/c otherwise a duplicate
 		//object is created when onClickB() is run.
 		$(this).off('click');
 		$(this).on('click', onClickB);
-		//send "this" to storage for later retrieval
-
 
 
 		function onClickB(){
 			$('.watched').append(this);
 			//send "this" to storage for later retrieval
 			console.log(this);
-			$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovie-http',
+			$.post('https://tiny-pizza-server.herokuapp.com/collections/charlesmovies-http',
 			{
 				movie: {
 	//I've tried various movie: values - movie: "movie", "this", etc...
-					movie: this,
+					movie: this.innerHTML,
 					}
 			}, 
 				function(movie){
